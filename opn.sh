3d946ab1ca05c6d2b297ee84c867ae6981f4dda2
Wamoya <juangrossodll@gmail.com>
2024-12-05 19:44:21 +0100

    New script: opn.sh

---
+++ opn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file=$(fzf --height=40% --header="Select the file to edit." --border --border-label="$PWD" --preview="cat {}")
+
+# Exit if $file is empty
+if [ -z "$file" ]; then
+        echo "Error: No file was selected."
+        exit 1
+fi
+
+# Exit if $file does not exist
+if [ ! -e "$file" ]; then
+	echo "Error: $file does not exist."
+	exit 1
+fi
+
+nvim $file
