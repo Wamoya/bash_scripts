dc1499814b82395c15341f3dcb8d02c553f33268
Wamoya <juangrossodll@gmail.com>
2025-03-01 16:18:09 +0100

    Add new gstatus.sh bash script that shows the status of all repos inside the repos folder

---
+++ gstatus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for dir in ~/repos/*; do
+    if [ -d "$dir" ]; then
+        echo $(basename "$dir")
+        (cd "$dir" && git status)
+        echo "========================================================================"
+    fi
+done
