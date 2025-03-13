1fb332570b93958bcbd35a0618945dddf3f64263
Wamoya <juangrossodll@gmail.com>
2025-03-13 10:04:40 +0100

    Add scraper.sh
    Add a simple web scraper script that receives a URL and then downloads
    its associated source code for later automatic filtering using grep.
    Then, it is automatically opened with the default browser.
    
    Note that it works with WSL.

---
+++ scraper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+local="/tmp/scraper.html"
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Use: $0 <URL>"
+    exit 1
+fi
+
+
+wget -q "$1" -O "$local"
+
+if [ $? -ne 0 ]; then
+    echo "Error when downloading from URL: $1"
+    exit 1
+fi
+
+
+
+grep -E '^(<h[1-6]>|<p>|<ul>|<ol>|<li>|<!DOCTYPE html>|<head>|</head>|<body>|</body>)' "$local" > "${local}_filtered"
+
+echo "Done!"
+filtered_file_path=$(wslpath -w "${local}_filtered")
+explorer.exe "$filtered_file_path"
+exit 0
